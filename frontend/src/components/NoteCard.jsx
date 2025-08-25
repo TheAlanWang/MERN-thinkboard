ef81b08df5ea167e2a4f6eb4d84944e4601a3271
Alanxwww <alanwang166@gmail.com>
2025-08-24 21:25:39 -0700

    frontend

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,10 +1,27 @@
 import { PenSquare, Trash2 } from "lucide-react";
 import { Link } from "react-router";
 import { formatDate } from "../lib/utils";
+import api from "../lib/axios";
+import toast from "react-hot-toast";
 
 import React from "react";
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+    const handleDelete = async (e, id) => {
+    e.preventDefault(); // get rid of the navigation behaviour
+
+    if (!window.confirm("Are you sure you want to delete this note?")) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id)); // get rid of the deleted one
+      toast.success("Note deleted successfully");
+    } catch (error) {
+      console.log("Error in handleDelete", error);
+      toast.error("Failed to delete note");
+    }
+  };
+
   return (
     <Link
       to={`/note/${note._id}`}
@@ -13,17 +30,23 @@ const NoteCard = ({ note }) => {
       <div className="card-body">
         <h3 className="card-title">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
+
         <div className="card-actions justify-between items-center mt-4">
           <span className="text-sm text-base-content/60">
             {formatDate(new Date(note.createdAt))}
           </span>
+          
           <div className="flex items-center gap-1">
-            <PenSquare className="size-4" />
-            <button className="btn btn-ghost btn-xs text-error">
+            <button className="btn btn-ghost btn-xs text-edit">
+              <PenSquare className="size-4" />
+            </button>
+            <button className="btn btn-ghost btn-xs text-error" onClick={(e) => handleDelete(e, note._id)}>
               <Trash2 className="size-4" />
             </button>
           </div>
+
         </div>
+
       </div>
     </Link>
   );
